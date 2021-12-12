c2688bf8ff3c61217fa54d4fef4b6e3d0181d139
AbdDaaboul <abrahman99@gmail.com>
2021-12-12 22:45:38 +0200

    making the user profile page as a component

---
+++ resources/views/components/Change-user-settings.blade.php
@@ -0,0 +1,65 @@
+<div class="col-md-8">
+    <div class="portlet light bordered">
+        <div class="portlet-title tabbable-line">
+            <div class="caption caption-md" style="margin-bottom: 20px; padding-left: 14px">
+                <i class="icon-globe theme-font hide"></i>
+                <span class="caption-subject font-blue-madison bold uppercase">Edit settings</span>
+            </div>
+        </div>
+        <div class="portlet-body">
+            <div class="col-lg-8 pb-5">
+                <form class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-fn">First Name</label>
+                            <input class="form-control" type="text" id="account-fn" value="Daniel" required="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-ln">Last Name</label>
+                            <input class="form-control" type="text" id="account-ln" value="Adams" required="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-email">E-mail Address</label>
+                            <input class="form-control" type="email" id="account-email"
+                                   value="daniel.adams@example.com" disabled="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-phone">Phone Number</label>
+                            <input class="form-control" type="text" id="account-phone"
+                                   value="+7 (805) 348 95 72" required="">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-pass">New Password</label>
+                            <input class="form-control" type="password" id="account-pass">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="account-confirm-pass">Confirm Password</label>
+                            <input class="form-control" type="password" id="account-confirm-pass">
+                        </div>
+                    </div>
+                    <div class="col-12" style="padding-left: 14px">
+                        <hr class="mt-2 mb-3">
+                        <div class="d-flex flex-wrap justify-content-between align-items-center">
+                            <button class="btn btn-style-1 btn-primary" type="button" data-toast=""
+                                    data-toast-position="topRight" data-toast-type="success"
+                                    data-toast-icon="fe-icon-check-circle" data-toast-title="Success!"
+                                    data-toast-message="Your profile updated successfuly.">Update Profile
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</div>
