7c523b73338775903511bec3856a690c17c095ad
fatima2ma <masa2fa@gmail.com>
2021-12-14 04:24:42 +0200

    edit contact view & design

--- resources/views/components/form/form-group.blade.php
+++ resources/views/components/form/form-group.blade.php
@@ -1,5 +1,5 @@
 @props(['type','label', 'name'])
-<div class="flex flex-col space-y-2 mb-4">
+<div class="flex flex-col mb-4">  <!--space-y-2-->
     <label for="{{$name}}" class="sr-only">{{$label}}</label>
     <label for="{{$name}}" class="">{{ucwords($label)}}</label>
     <input id="{{$name}}" name="{{$name}}" type="{{$type}}" autocomplete="{{$name}}" value="{{($type == 'password')? '' : old($name)}}" required class="appearance-none rounded-none relative block h-12 w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="{{ucWords($label)}}">
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,34 @@
 <x-layout>
     <x-page-title class="self-start my-4">Connect</x-page-title>
-    <div class="flex">
-        <p class="text-sm text-gray-500">welcome one more time, please Enter your Information.</p>
-        <form class="mt-8 space-y-6" action="" method="POST">
+    <div class="flex w-full my-16 flex-col md:flex-row bg-dotted-lg">
+        <div class="w-full md:w-3/5 mx-8 mb-8">
+            <h2 class="text-base font-medium text-2xl text-gray-900 dark:text-white">Get in touch</h2>
+            <p class="mt-6 text-base font-medium text-gray-600 dark:text-gray-400 sm:text-lg md:text-lg">
+                How can we help you?
+            </p>
+            <p class="mt-6 text-base font-medium text-gray-600 dark:text-gray-400 sm:text-lg md:text-lg">
+                We'd love to hear from you, we are always listening, just drop us a line.
+            </p>
+            <p class="mt-6 text-base font-medium text-gray-600 dark:text-gray-400 sm:text-lg md:text-lg">
+                <span class="text-red-500 underline">Note</span>
+                Please login before send your message.
+            </p>
+        </div>
+        <form class="w-full md:w-4/5 bg-gray-100 z-10 shadow-lg p-4" action="" method="POST">
             @csrf
             <input type="hidden" name="remember" value="true">
-            <div class="rounded-md shadow-sm -space-y-px">
+            <div class="rounded-md shadow-sm">
                 <x-form.form-group type="text" label="Message title" name="message"></x-form.form-group>
+                <div class="flex flex-col space-y-2 mb-8">
+                    <label for="body" class="sr-only">Message</label>
+                    <label for="body" class="">Message</label>
+                    <textarea id="body" name="body" autocomplete="body" value="{{old('body')}}" required rows="4" class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Message"></textarea>
+                    @error('body')
+                        <p class="text-red-500 text-sm mt-1">{{$message}}</p>
+                    @enderror
+                </div>
             </div>
+            <x-form.submit-btn>Send</x-form.submit-btn>
         </form>
     </div>
     @include('_contact')
